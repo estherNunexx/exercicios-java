34d4d55484f333ae2ef831cb85d2143765f4f20e
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-08-26 11:30:31 -0300

    programa em Java que leia um número inteiro e informe se ele é par ou ímpar.

---
+++ Exercicios02/ParOuImpar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+       try (Scanner entrada = new Scanner(System.in)) {
+        System.out.print("Digite um número inteiro: ");
+        int numero = entrada.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par");
+        } else {
+            System.out.println("O número é ímpar");
+        }
+
+        entrada.close();
+    }
+}
+
+}
